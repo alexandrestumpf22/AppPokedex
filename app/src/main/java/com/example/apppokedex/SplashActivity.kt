7c9957a4c8c72b9a07b31b28426abd96b4465aea
Alexandre <portfolio.alexandrestumpf@gmail.com>
2022-04-10 14:53:08 -0300

    criando uma tela splash com verificação de internet

---
+++ app/src/main/java/com/example/apppokedex/SplashActivity.kt
@@ -0,0 +1,60 @@
+package com.example.apppokedex
+
+import android.app.ListActivity
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if (checkForInternet(this)){
+            changeToList()
+        }
+        else{
+            Toast.makeText(this, R.string.erro_internet, Toast.LENGTH_LONG).show()
+            finish()
+        }
+    }
+
+    fun changeToList(){
+        val intent = Intent(this, com.example.apppokedex.ListActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            intent.change()
+            finish()
+        },5000)
+    }
+
+    fun Intent.change(){
+        startActivity(this)
+        finish()
+    }
+
+    private fun checkForInternet(context: Context): Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when{
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+                else -> false
+            }
+        }
+        else{
+            @Suppress("DEPRECATION") val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION") return networkInfo.isConnected
+        }
+    }
+}
